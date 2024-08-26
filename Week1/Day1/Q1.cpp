12eac13c61ca49ca053fb4e8d11b241320a05a4d
Rajeev Ranjan <rjranjan2112@gmail.com>
2024-08-26 19:29:22 +0530

    Day1 Q1 String Concate

---
+++ Week1/Day1/Q1.cpp
@@ -0,0 +1,16 @@
+// Question 1
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    string s1, s2;
+    cout << "Enter the first String: " << endl;
+    cin >> s1;
+    cout << "Enter the second String: " << endl;
+    cin >> s2;
+
+    cout << "Concatenated String: " << endl;
+    cout << s1 + s2;
+    
+return 0;
+}
